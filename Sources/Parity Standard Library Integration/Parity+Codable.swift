de197bdfacf1bf8c3f1b7bc0f6cad32f0a0a2f46
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-27 20:02:17 +0200

    Implement Parity Codable conformance manually
    Synthesis is unavailable in an extension outside the declaring file, so
    the cross-module extension now encodes and decodes the case name as a
    single string value.

--- Sources/Parity Standard Library Integration/Parity+Codable.swift
+++ Sources/Parity Standard Library Integration/Parity+Codable.swift
@@ -1,5 +1,27 @@
 import Parity
 
 #if !hasFeature(Embedded)
-    extension Parity: Codable {}
+    extension Parity: Codable {
+
+        public init(from decoder: any Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            switch try container.decode(String.self) {
+            case "even": self = .even
+            case "odd": self = .odd
+            case let value:
+                throw DecodingError.dataCorruptedError(
+                    in: container,
+                    debugDescription: "Invalid Parity value: \(value)"
+                )
+            }
+        }
+
+        public func encode(to encoder: any Encoder) throws {
+            var container = encoder.singleValueContainer()
+            switch self {
+            case .even: try container.encode("even")
+            case .odd: try container.encode("odd")
+            }
+        }
+    }
 #endif
